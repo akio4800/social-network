11dce2d99cc16e80acad0446df132628ad2d955c
Ramón Huidobro <hola@ramonh.dev>
2024-04-15 12:22:51 +0200

    Use the API to show the posts

--- apps/ui/src/app/app.tsx
+++ apps/ui/src/app/app.tsx
@@ -1,12 +1,32 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-import styles from './app.module.css';
+import { useEffect, useState } from 'react';
 
-import NxWelcome from './nx-welcome';
+interface Post {
+  id: number;
+  content: string;
+}
 
 export function App() {
+  const [posts, setPosts] = useState([]);
+  // get the posts from the `feed` nx app and display them
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await fetch('http://localhost:3000');
+      const data = await response.json();
+      setPosts(data);
+    };
+    fetchPosts();
+  }, []);
   return (
     <div>
-      <NxWelcome title="ui" />
+      <h1>Welcome to ui!</h1>
+      <h2>Posts</h2>
+      <ul>
+        {posts.map((post: Post) => (
+          <li key={post.id}>{post.content}</li>
+        ))}
+      </ul>
     </div>
   );
 }
