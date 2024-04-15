315e6439af6dcce749c5f9a768321f5b15c7ac7b
Ramón Huidobro <hola@ramonh.dev>
2024-04-15 10:39:34 +0200

    Remove comment

--- apps/feed/src/main.ts
+++ apps/feed/src/main.ts
@@ -8,8 +8,6 @@ const port = process.env.PORT ? Number(process.env.PORT) : 3000;
 const app = express();
 
 app.get('/', (req, res) => {
-  // return the last 10 posts as JSON
-
   const posts = prisma.post.findMany({
     take: 10,
     orderBy: { createdAt: 'desc' },
