8426b9e01041ee393961980807ec4b64aaa7beb6
Ramón Huidobro <hola@ramonh.dev>
2024-04-15 10:39:13 +0200

    Implement feed

--- apps/feed/src/main.ts
+++ apps/feed/src/main.ts
@@ -1,12 +1,21 @@
+import { PrismaClient } from '@prisma/client';
 import express from 'express';
 
 const host = process.env.HOST ?? 'localhost';
+const prisma = new PrismaClient();
 const port = process.env.PORT ? Number(process.env.PORT) : 3000;
 
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send({ message: 'Hello API' });
+  // return the last 10 posts as JSON
+
+  const posts = prisma.post.findMany({
+    take: 10,
+    orderBy: { createdAt: 'desc' },
+  });
+
+  res.json(posts);
 });
 
 app.listen(port, host, () => {
